158715c3ea21e17b4c68ca7de7cd001e6507f10b
ArshBains <56476189+ArshBains@users.noreply.github.com>
2020-04-26 19:32:52 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,46 @@
-var number=0;function calculate(){for(var e=[],t=[],n=0,l=0,a=[],r=document.getElementsByName("grade"),u=document.getElementsByName("credits"),i=0;i<r.length&&i<u.length;i++){if(""==r[i].value||""==u[i].value)return alert("Please fill all inputs fields. (Subject name is optional)"),!1;t[i]=Number(r[i].value),e[i]=Number(u[i].value)}for(i=0;i<e.length;i++)a[i]=e[i]*t[i];for(i=0;i<e.length;i++)n+=e[i],l+=a[i];return(l/n).toFixed(2)}$(".new").on("click",function(){var e='<div class="row" id="rb'+ ++number+'"> <input type="text" name="subject" value="" placeholder="Subject name"> <input type="number" name="grade" value="" placeholder="Grade Point*"> <input type="number" name="credits" value="" placeholder="Credits*"> <button type="button" class="bin" id="b'+number+'"><i class="fa fa-trash" aria-hidden="true"></i></button> </div>';$(".new").before(e),$("#b"+number).on("click",function(){var e=this.id;$("#r"+e).remove()})}),$(".calc").on("click",function(){result=calculate(),result&&$("h1.result").text("SGPA: "+result)});
+// ----------add new div rows and also add event listener to the delete button in each div----------
+$('.new').on('click', function() {
+
+  var newEle = '<div class="row"> <input type="text" name="subject" value="" placeholder="Subject name"> <input type="number" name="grade" min="0" max="10" value="" placeholder="Grade Point*"> <input type="number" name="credits" min="0" value="" placeholder="Credits*"> <button type="button" class="bin"><i class="fa fa-trash" aria-hidden="true"></i></button> </div>';
+  $('.new').before(newEle);
+  $('.bin').on('click', function() {
+    // var deleteid=this.id;
+    // $('#r'+deleteid).remove
+    this.parentNode.remove();
+  })
+})
+
+// ----------call to calculate result if any input is empty then show alert---------------
+$('.calc').on('click', function() {
+  result = calculate();
+  if (result) {
+    $('h1.result').text("SGPA: " + result);
+  }
+})
+
+// --------read input>convert it into array>calculate SGPA and return value----------
+function calculate() {
+  var credits = [];
+  var grade = [];
+  var totalcr = 0;
+  var ercr = 0;
+  var combcr = [];
+  var gr = document.getElementsByName("grade");
+  var cr = document.getElementsByName("credits");
+  for (var i = 0; i < gr.length && i < cr.length; i++) {
+    if (gr[i].value == "" || cr[i].value == "") {
+      alert("Please fill all inputs fields. (Subject name is optional)");
+      return false;
+    }
+    grade[i] = Number(gr[i].value);
+    credits[i] = Number(cr[i].value);
+  }
+  for (var i = 0; i < credits.length; i++) {
+    combcr[i] = credits[i] * grade[i]
+  }
+  for (var i = 0; i < credits.length; i++) {
+    totalcr += credits[i];
+    ercr += combcr[i]
+  }
+  return (ercr / totalcr).toFixed(2);
+}
